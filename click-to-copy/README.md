4fd342261a84e6a584027bc7bfab98488aeaf753
Vedant Gonnade <vedantgonnade@outlook.com>
2025-02-04 15:56:12 +0100

    created click-to-copy project

---
+++ click-to-copy/README.md
@@ -0,0 +1,19 @@
+# modal-popup
+
+This project is to create a small popup on screen when a button is clicked.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+The final project will look like this:
+
+![alt text](image.png)
---
+++ click-to-copy/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <main
+    class="flex h-screen items-center justify-center bg-blue-500 text-white"
+  >
+    <div class="flex flex-col rounded-lg p-8">
+      <p>Get 25% Discount with this coupon</p>
+      <div class="py-3">
+        <input
+          type="text"
+          class="mr-4 rounded border bg-white py-1 text-center text-black"
+          v-model="coupon"
+        />
+        <button
+          class="cursor-pointer rounded border-1 px-3 py-1 text-center hover:bg-blue-800"
+          @click="copyToClipboard"
+        >
+          {{ copyButtonText }}
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      coupon: "",
+      copyButtonText: "Copy",
+    };
+  },
+  methods: {
+    copyToClipboard() {
+      navigator.clipboard.writeText(this.coupon);
+      this.copyButtonText = "Copied!";
+      setTimeout(() => {
+        this.copyButtonText = "Copy";
+        this.coupon = "";
+      }, 3000);
+    },
+  },
+};
+</script>
